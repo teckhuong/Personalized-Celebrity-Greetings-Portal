04c1f8f42c65e20e0e1cd40ef093f0c04ebbcafc
teckhuong <55531139+teckhuong@users.noreply.github.com>
2021-10-04 15:26:30 +0800

    Add files via upload

---
+++ CelebPortal/CelebPortal/howto.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+   <head>
+   <?php include "header.php";?>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="stylehowto.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+      <div class="cards">
+         <h2 class="header">
+            How to Order
+         </h2>
+         <div class="services">
+            <div class="content content-1">
+               <div class="fas fa-star"></div>
+               <h2>
+                  Celebrity
+               </h2>
+               <p>
+                  Pick your favourite celebrity from our roster
+               </p>
+            </div>
+            <div class="content content-2">
+               <div class="fas fa-file-alt"></div>
+               <h2>
+                  Order Form
+               </h2>
+               <p>
+               Fill in the form correctly which is available next to celebrity
+               </p>
+            </div>
+            <div class="content content-3">
+               <div class="fas fa-envelope"></div>
+               <h2>
+                Order Done
+               </h2>
+               <p>
+               After ordering please wait for celebrity script acceptance
+               </p>
+            </div>
+         </div>
+      </div>
+      <div class="cards">
+         <div class="services">
+            <div class="content content-4">
+               <div class="fas fa-money-bill"></div>
+               <h2>
+                  Payment
+               </h2>
+               <p>
+               If confirmed an email will be received with payment link
+               </p>
+            </div>
+            <div class="content content-5">
+               <div class="fas fa-frown"></div>
+               <h2>
+                 Sorry
+               </h2>
+               <p>
+                If not confirmed an email will be received stating the reason
+               </p>
+            </div>
+            <div class="content content-6">
+               <div class="fab fa-youtube"></div>
+               <h2>
+                  Video
+               </h2>
+               <p>
+               After transaction is confirmed you will receive your video
+               </p>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
