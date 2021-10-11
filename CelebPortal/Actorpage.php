ff5558b8d0a794ec5d86341a44bc8e1ceed6d06f
badphilip <55310704+badphilip@users.noreply.github.com>
2021-10-11 12:31:22 +0800

    Add files via upload

---
+++ CelebPortal/Actorpage.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <title>TikTokStars</title>
+        <?php include "header.php";?>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+        <link rel="stylesheet" type="text/css" href="categorypage.css">
+        </head>
+        <body >    
+            <section class="secondrow">
+                <div class="pagetitle">
+                            <h1><span class="fas fa-film"></span> Actors</h1>
+                    </div>
+                    <div class="container">
+                        <!-- image row start -->
+                        <div class="row">
+                            <!-- image start -->
+                            <div class="cimage">
+                                    <img src="youtuberimage/laowu/laowu.png" alt="">
+                                    <div class="details">
+                                        <h2>LAO WU</h2>
+                                        <p>Youtuber who has been around for 10 years. mjor on streaming video games</p>
+                                        <div class="more">
+                                                <a href="#" class="read-more">Bring me there</a>
+                                        </div>
+                                        <div class="icon-links">
+                                                <a href="#"></a>
+                                        </div>
+                                    </div>
+                            </div>
+                            <!-- image end -->
+                            <!-- image start -->
+                            <div class="cimage">
+                                    <img src="youtuberimage/laowu/laowu.png" alt="">
+                                    <div class="details">
+                                        <h2>LAO WU</h2>
+                                        <p>Youtuber who has been around for 10 years. mjor on streaming video games</p>
+                                        <div class="more">
+                                                <a href="#" class="read-more">Bring me there</a>
+                                        </div>
+                                        <div class="icon-links">
+                                                <a href="#"></a>
+                                        </div>
+                                    </div>
+                            </div>
+                            <!-- image end -->
+                            <!-- image start -->
+                            <div class="cimage">
+                                    <img src="youtuberimage/laowu/laowu.png" alt="">
+                                    <div class="details">
+                                        <h2>LAO WU</h2>
+                                        <p>Youtuber who has been around for 10 years. mjor on streaming video games</p>
+                                        <div class="more">
+                                                <a href="#" class="read-more">Bring me there</a>
+                                        </div>
+                                        <div class="icon-links">
+                                                <a href="#"></a>
+                                        </div>
+                                    </div>
+                            </div>
+                            <!-- image end -->
+                            <!-- image start -->
+                            <div class="cimage">
+                                    <img src="youtuberimage/laowu/laowu.png" alt="">
+                                    <div class="details">
+                                        <h2>LAO WU</h2>
+                                        <p>Youtuber who has been around for 10 years. mjor on streaming video games</p>
+                                        <div class="more">
+                                                <a href="#" class="read-more">Bring me there</a>
+                                        </div>
+                                        <div class="icon-links">
+                                                <a href="#"></a>
+                                        </div>
+                                    </div>
+                            </div>
+                            <!-- image end -->
+                            <!-- image start -->
+                            <div class="cimage">
+                                    <img src="youtuberimage/laowu/laowu.png" alt="">
+                                    <div class="details">
+                                        <h2>LAO WU</h2>
+                                        <p>Youtuber who has been around for 10 years. mjor on streaming video games</p>
+                                        <div class="more">
+                                                <a href="#" class="read-more">Bring me there</a>
+                                        </div>
+                                        <div class="icon-links">
+                                                <a href="#"></a>
+                                        </div>
+                                    </div>
+                            </div>
+                            <!-- image end -->
+                        </div>
+
+                    </div>
+            </section>    
+            <!-- footer -->
+            <section class="d-flex-r justify-content-space-around p-1 bg-grey" id="social">
+                    <ul class="footerforhome d-flex-c" id="footer">
+                        <p class="wsnc">For more: </p>
+                        <li class="rnc"><a href="#" >Request New Celebrity</a></li>
+                        <li class="rnc"><a href="#" >How to Order?</a></li>
+                        <li class="rnc"><a href="#" >Contact Us</a></li>
+                                </br>
+                        <p>&copy; celebportal.com | Designed by Farmer Team</p>
+                    </ul>
+                    <ul class="d-flex-r">
+                        
+                        <li>
+                        <p>Follow us for more!</p>
+                        <p>Our Social Media Platform:</p>
+                            <a href="face"><i class="fab fa-facebook"></i></a>
+                            <a href="twi"><i class="fab fa-twitter"></i></a>
+                            <a href="ins"><i class="fab fa-instagram"></i></a>
+                            <a href="you"><i class="fab fa-youtube"></i></a>
+                        </li>            
+                    </ul>
+        
+            </section>
+        </body>
+    
+</html>
