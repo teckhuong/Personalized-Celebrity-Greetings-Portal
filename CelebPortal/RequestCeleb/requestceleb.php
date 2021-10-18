2b5ab1804319e626b30e131451c4c4b083168e69
robin4177 <55526348+robin4177@users.noreply.github.com>
2021-10-18 22:26:00 +0530

    Add files via upload

---
+++ CelebPortal/RequestCeleb/requestceleb.php
@@ -0,0 +1,44 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Request Form</title>
+  <link rel="stylesheet" type="text/css" href="stylerequest.css">
+</head>
+<body>
+<br><br>
+  <div class="header">
+  	<h2>Request a Celebrity</h2>
+  </div>
+  <form method="post" action="requestceleb.php">
+  	<?php include('errors.php'); ?>
+	  <?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      </div>
+  	<?php endif ?>
+  	<div class="input-group">
+  	  <label>Your Email</label>
+  	  <input type="email" name="email">
+  	</div>
+  	<div class="input-group">
+  	  <label>Celebrity Name</label>
+  	  <input type="text" name="celebname">
+  	</div>
+  	<div class="input-group">
+  	  <label>Celebrity Country</label>
+  	  <input type="text" name="celebcountry">
+  	</div>
+	  <div class="input-group">
+  	  <label>Comments</label>
+  	  <input type="message" name="comments">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Request</button>
+  	</div>
+  </form>
+</body>
+</html>
