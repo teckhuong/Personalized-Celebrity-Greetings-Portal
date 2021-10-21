183d8c2ea7dad5169035f024eae060e38d50b052
teckhuong <55531139+teckhuong@users.noreply.github.com>
2021-10-21 15:09:22 +0800

    Profile

--- CelebPortal/CelebrityProfile.php
+++ CelebPortal/CelebrityProfile.php
@@ -1,14 +1,57 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <title>TikTokStars</title>
+        <title>Profile page</title>
         <?php include "header.php";?>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+        <link rel="stylesheet" type="text/css" href="profile.css">
         <link rel="stylesheet" type="text/css" href="categorypage.css">
         </head>
      <body > 
+        <section class="profilerow">
+        <div class="profile-container">
+            <div class="profile-content">
+                <img class="profileSlide" scr="youtuberimage/laowu/laowu.png" alt="LAO WU" />
+                <img class="profileSlide" scr="youtuberimage/laowu/laowu.png" alt="LAO WU" />
+                <img class="profileSlide" scr="youtuberimage/laowu/laowu.png" alt="LAO WU" />
+
+                <div class="profile-row-padding w3-section">
+                    <div class="profile-col s4">
+                        <img class="demo profile-opacity profile-hover-opacity-off" scr="youtuberimage/laowu/laowu.png" onclick="currDiv(1)" />
+                    </div>
+                    <div class="profile-col s4">
+                        <img class="demo profile-opacity profile-hover-opacity-off" scr="youtuberimage/laowu/laowu.png" onclick="currDiv(2)" />
+                    </div>
+                    <div class="profile-col s4">
+                        <img class="demo profile-opacity profile-hover-opacity-off" scr="youtuberimage/laowu/laowu.png" onclick="currDiv(3)" />
+                    </div>
+                </div>
+            </div>
+
+        <script>
+            function currDiv(i){
+                showDivs(slideIndex = i);
+            }
+
+            function showDivs(n) {
+                var x;
+                var y = document.getElementsByClassName("profileSlide");
+                var dots = document.getElementsByClassName("demo");
+                if (n > y.length) {slideIndex = 1;}
+                if(n < 1) {slideIndex = y.length;}
+                for(x = 0; x < y.length; x++) {
+                    y[x].style.display = "none";
+                }
+                for(x = 0; x < dots.length; x++) {
+                    dots[x].className = dots[x].className.replace(" profile-opacity-off", "")
+                }
+                y[slideIndex-1].style.display = "block";
+                dots[slideIndex-1].className += " profile-opacity-off";
+            }
+        </script>
+        </div>
          <div class="slideshow"></div>
              <!-- footer -->
             <section class="d-flex-r justify-content-space-around p-1 bg-grey" id="social">
@@ -21,7 +64,6 @@
                     <p>&copy; celebportal.com | Designed by Farmer Team</p>
                 </ul>
                 <ul class="d-flex-r">
-                        
                     <li>
                     <p>Follow us for more!</p>
                     <p>Our Social Media Platform:</p>
