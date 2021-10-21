105db53501aeb4b9157d5e6a56aaae4ae24cb9ed
teckhuong <55531139+teckhuong@users.noreply.github.com>
2021-10-21 14:21:40 +0800

    Create CelebrityProfile.php

---
+++ CelebPortal/CelebrityProfile.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <title>TikTokStars</title>
+        <?php include "header.php";?>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+        <link rel="stylesheet" type="text/css" href="categorypage.css">
+        </head>
+     <body > 
+         <div class="slideshow"></div>
+             <!-- footer -->
+            <section class="d-flex-r justify-content-space-around p-1 bg-grey" id="social">
+                <ul class="footerforhome d-flex-c" id="footer">
+                    <p class="wsnc">For more: </p>
+                    <li class="rnc"><a href="#" >Request New Celebrity</a></li>
+                    <li class="rnc"><a href="#" >How to Order?</a></li>
+                    <li class="rnc"><a href="#" >Contact Us</a></li>
+                         </br>
+                    <p>&copy; celebportal.com | Designed by Farmer Team</p>
+                </ul>
+                <ul class="d-flex-r">
+                        
+                    <li>
+                    <p>Follow us for more!</p>
+                    <p>Our Social Media Platform:</p>
+                        <a href="face"><i class="fab fa-facebook"></i></a>
+                        <a href="twi"><i class="fab fa-twitter"></i></a>
+                        <a href="ins"><i class="fab fa-instagram"></i></a>
+                        <a href="you"><i class="fab fa-youtube"></i></a>
+                     </li>            
+                </ul>
+        
+            </section>
+     </body>
+    
+</html>
