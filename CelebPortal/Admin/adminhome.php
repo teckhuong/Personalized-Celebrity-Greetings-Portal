07a3cc31acfd3daf45d75ef0dd94513f869f589f
badphilip <55310704+badphilip@users.noreply.github.com>
2021-10-25 16:54:32 +0800

    Add Admin UI
    Admin UI done 20%

---
+++ CelebPortal/Admin/adminhome.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Homepage</title>
+    <link rel="stylesheet" type="text/css" href="adminhome.css">
+</head>
+<body>
+    <div class="admincontainer">
+        <div class="adminnavigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class="title">Celebrity Portal Admin Panel</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                        <span class="title">Homepage</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
+                        <span class="title">User Profile Management</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
+                        <span class="title">Agent Profile Management</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="document-outline"></ion-icon></span>
+                        <span class="title">Quotation Form</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="document-outline"></ion-icon></span>
+                        <span class="title">Add New Product</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="person-add-outline"></ion-icon></span>
+                        <span class="title">Add New Agent</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?logout='1'">
+                        <span class="icon"><ion-icon name="log-out-outline"></ion-icon></span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- main -->
+        <div class="main">
+            <div class="topbar">
+                <div class="toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+                <div class="adminname">
+                    <!-- username -->
+                    <?php
+                   // if(isset($_SESSION["adminid"])){
+                   //     echo $_SESSION["adminfullname"];
+                        //echo "<li><a href='index.php?logout='1''>Log out</a></li>";
+                    //}
+                    //?>
+                    <span>Theressa Wong</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script>
+        //add hovered class in selected list item
+        let list = document.querySelectorAll('.adminnavigation li');
+        function activeLink(){
+            list.forEach((item)=>
+            item.classList.remove('hovered'));
+            this.classList.add('hovered');
+        }
+        list.forEach((item)=>
+        item.addEventListener('mouseover',activeLink));
+    </script>
+</body>
+</html>
